74a7673719a0c7739466ba3ae87c87a8b67709d9
Dr Michael M. Akram <michael.akram@gmail.com>
2025-04-20 23:21:39 +1000

    Starts implementing home page feed.

--- lib/partners_web/live/home/home_live.ex
+++ lib/partners_web/live/home/home_live.ex
@@ -5,14 +5,43 @@ defmodule PartnersWeb.Home.HomeLive do
 
   @threshold 2000
 
+  # Region codes ISO 3166-2
+  @flags %{
+    "AU-NSW" => "New_South_Wales.svg",
+    "AU-QLD" => "Queensland.svg",
+    "AU-SA" => "South_Australia.svg",
+    "AU-TAS" => "Tasmania.svg",
+    "AU-VIC" => "Victoria.svg",
+    "AU-WA" => "Western_Australia.svg",
+    "AU-ACT" => "Australian_Capital_Territory.svg",
+    "AU-NT" => "Northern_Territory.svg"
+  }
+
   @impl true
   def mount(_params, _session, socket) do
     socket =
       socket
       |> assign(page_title: "Home")
       |> assign_new(:current_scope, fn -> %{} end)
+      |> stream(:feed, [])
+      |> stream_insert(
+        :feed,
+        %{
+          username: "Anon",
+          region_name: "Queensland",
+          region_code_code: "AU-QLD",
+          time_zone: "Australian Eastern Standard Time",
+          flag_url: ~p"/images/flags/#{@flags["AU-QLD"]}",
+          action: "Viewing",
+          id: :os.system_time(:seconds) |> Integer.to_string()
+        },
+        at: 0
+      )
 
     # |> put_flash(:success, "Welcome to Phoenix LiveView!")
+    if connected?(socket) do
+      PartnersWeb.Endpoint.subscribe("home")
+    end
 
     {:ok, socket}
   end
@@ -39,7 +68,14 @@ defmodule PartnersWeb.Home.HomeLive do
       ) do
     IO.inspect(response, label: "IP Data")
     IO.inspect(responseHeaders, label: "Response Headers")
+
+    region_name = response["location"]["region"]["name"]
+    region_code = response["location"]["region"]["code"]
+    time_zone = response["time_zone"]["name"]
+    flag_url = @flags[region_code]
+
     maybe_send_admin_email(responseHeaders["ipregistry-credits-remaining"])
+
     {:noreply, socket}
   end
 
@@ -47,6 +83,23 @@ defmodule PartnersWeb.Home.HomeLive do
   @impl true
   def handle_event("ip_registry_data", %{"status" => "OK", "result" => result}, socket) do
     IO.inspect(result, label: "Response Data")
+
+    socket =
+      socket
+      |> stream_insert(
+        :feed,
+        %{
+          username: "Anon",
+          region_name: "Queensland",
+          region_code_code: "AU-QLD",
+          time_zone: "Australian Eastern Standard Time",
+          flag_url: ~p"/images/flags/#{@flags["AU-QLD"]}",
+          action: "Viewing",
+          id: :os.system_time(:seconds) |> Integer.to_string()
+        },
+        at: 1
+      )
+
     {:noreply, socket}
   end
 
@@ -70,3 +123,109 @@ defmodule PartnersWeb.Home.HomeLive do
     end
   end
 end
+
+# Response Data: %{
+#   "carrier" => %{"mcc" => nil, "mnc" => nil, "name" => nil},
+#   "company" => %{
+#     "domain" => "wideband.net.au",
+#     "name" => "Aussie Broadband",
+#     "type" => "isp"
+#   },
+#   "connection" => %{
+#     "asn" => 4764,
+#     "domain" => "wideband.net.au",
+#     "organization" => "Wideband Networks Pty LTD",
+#     "route" => "117.20.68.0/22",
+#     "type" => "isp"
+#   },
+#   "currency" => %{
+#     "code" => "AUD",
+#     "format" => %{
+#       "decimal_separator" => ".",
+#       "group_separator" => ",",
+#       "negative" => %{"prefix" => "-$", "suffix" => ""},
+#       "positive" => %{"prefix" => "$", "suffix" => ""}
+#     },
+#     "name" => "Australian Dollar",
+#     "name_native" => "Australian Dollar",
+#     "plural" => "Australian dollars",
+#     "plural_native" => "Australian dollars",
+#     "symbol" => "A$",
+#     "symbol_native" => "$"
+#   },
+#   "hostname" => nil,
+#   "ip" => "117.20.68.135",
+#   "location" => %{
+#     "city" => "Milton",
+#     "continent" => %{"code" => "OC", "name" => "Oceania"},
+#     "country" => %{
+#       "area" => 7686850,
+#       "borders" => [],
+#       "calling_code" => "61",
+#       "capital" => "Canberra",
+#       "code" => "AU",
+#       "flag" => %{
+#         "emoji" => "🇦🇺",
+#         "emoji_unicode" => "U+1F1E6 U+1F1FA",
+#         "emojitwo" => "https://cdn.ipregistry.co/flags/emojitwo/au.svg",
+#         "noto" => "https://cdn.ipregistry.co/flags/noto/au.png",
+#         "twemoji" => "https://cdn.ipregistry.co/flags/twemoji/au.svg",
+#         "wikimedia" => "https://cdn.ipregistry.co/flags/wikimedia/au.svg"
+#       },
+#       "languages" => [
+#         %{"code" => "en", "name" => "English", "native" => "English"}
+#       ],
+#       "name" => "Australia",
+#       "population" => 26658948,
+#       "population_density" => 3.47,
+#       "tld" => ".au"
+#     },
+#     "in_eu" => false,
+#     "language" => %{"code" => "en", "name" => "English", "native" => "English"},
+#     "latitude" => -27.47421,
+#     "longitude" => 153.0038,
+#     "postal" => "4064",
+#     "region" => %{"code" => "AU-QLD", "name" => "Queensland"}
+#   },
+#   "security" => %{
+#     "is_abuser" => false,
+#     "is_anonymous" => false,
+#     "is_attacker" => false,
+#     "is_bogon" => false,
+#     "is_cloud_provider" => false,
+#     "is_proxy" => false,
+#     "is_relay" => false,
+#     "is_threat" => false,
+#     "is_tor" => false,
+#     "is_tor_exit" => false,
+#     "is_vpn" => false
+#   },
+#   "time_zone" => %{
+#     "abbreviation" => "AEST",
+#     "current_time" => "2025-04-20T16:51:45+10:00",
+#     "id" => "Australia/Brisbane",
+#     "in_daylight_saving" => false,
+#     "name" => "Australian Eastern Standard Time",
+#     "offset" => 36000
+#   },
+#   "type" => "IPv4",
+#   "user_agent" => %{
+#     "device" => %{
+#       "brand" => nil,
+#       "name" => "Linux Desktop",
+#       "type" => "desktop"
+#     },
+#     "engine" => %{
+#       "name" => "Gecko",
+#       "type" => "browser",
+#       "version" => "137.0",
+#       "version_major" => "137"
+#     },
+#     "header" => "Mozilla/5.0 (X11; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0",
+#     "name" => "Firefox",
+#     "os" => %{"name" => "Linux", "type" => "desktop", "version" => nil},
+#     "type" => "browser",
+#     "version" => "137.0",
+#     "version_major" => "137"
+#   }
+# }
